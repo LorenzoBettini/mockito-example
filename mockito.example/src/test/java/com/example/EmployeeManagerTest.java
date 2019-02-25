69afc8936db1b6963835eb5f9191c2687df497f1
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-25 15:52:56 +0100

    first test, does not compile

--- mockito.example/src/test/java/com/example/EmployeeManagerTest.java
+++ mockito.example/src/test/java/com/example/EmployeeManagerTest.java
@@ -1,5 +1,16 @@
 package com.example;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
 public class EmployeeManagerTest {
+
+	@Test
+	public void testPayEmployeesWhenNoEmployeesArePresent() {
+		EmployeeManager employeeManager = new EmployeeManager();
+		int numberOfPayments = employeeManager.payEmployees();
+		assertThat(numberOfPayments).isEqualTo(0);
+	}
 
 }
