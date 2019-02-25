46ceadf35a950458b18778994105665a7ade5cf2
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-25 16:05:01 +0100

    stub findAll

--- mockito.example/src/test/java/com/example/EmployeeManagerTest.java
+++ mockito.example/src/test/java/com/example/EmployeeManagerTest.java
@@ -3,6 +3,9 @@ package com.example;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -26,6 +29,9 @@ public class EmployeeManagerTest {
 
 	@Test
 	public void testPayEmployeesWhenOneEmployeeIsPresent() {
+		List<Employee> employees = new ArrayList<>();
+		employees.add(new Employee("1", 1000));
+		when(employeeRepository.findAll()).thenReturn(employees);
 		assertThat(employeeManager.payEmployees()).isEqualTo(1);
 	}
 }
