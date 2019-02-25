7cc02633191ede3ae6f498b0305cf01d5303ee52
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-25 16:00:32 +0100

    added constructor with EmployeeRepository

--- mockito.example/src/main/java/com/example/EmployeeManager.java
+++ mockito.example/src/main/java/com/example/EmployeeManager.java
@@ -2,6 +2,12 @@ package com.example;
 
 public class EmployeeManager {
 
+	private EmployeeRepository employeeRepository;
+
+	public EmployeeManager(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
+
 	public int payEmployees() {
 		return 0;
 	}
