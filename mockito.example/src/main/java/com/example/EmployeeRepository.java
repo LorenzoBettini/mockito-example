4efed3741ca07ea2809fe00b7365a8d7111e79bc
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-02-18 11:20:15 +0100

    Revert "example of bad stubbing with answer"
    This reverts commit 75a3990c4e06e63bfd01551594deb54be6a0c0d9.

--- mockito.example/src/main/java/com/example/EmployeeRepository.java
+++ mockito.example/src/main/java/com/example/EmployeeRepository.java
@@ -1,11 +1,9 @@
 package com.example;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface EmployeeRepository {
 
 	List<Employee> findAll();
 
-	Optional<Employee> findById(String id);
 }
